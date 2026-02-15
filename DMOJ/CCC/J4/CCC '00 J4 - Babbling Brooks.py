43d2e6d8dd9c7d6b661eb0f871b2afb39caabad9
Roy Lu <Roylu0404@gmail.com>
2026-02-15 11:00:17 -0500

    asd

---
+++ DMOJ/CCC/J4/CCC '00 J4 - Babbling Brooks.py
@@ -0,0 +1,34 @@
+# SAME AS CCC '00 J4 - Babbling Brooks
+
+import sys
+
+n = int(sys.stdin.readline())
+
+streams = [int(sys.stdin.readline()) for _ in range(n)]
+
+while True:
+    command = int(sys.stdin.readline())
+
+    if command == 77:
+        break
+
+    if command == 99:
+
+        m = int(sys.stdin.readline()) - 1
+        percentage = int(sys.stdin.readline()) 
+
+        flow = streams[m]
+        left_flow = flow * (percentage/100)
+        right_flow = flow - left_flow
+
+        streams[m] = left_flow
+        streams.insert(m + 1, right_flow)
+
+    elif command == 88: # join
+        m = int(sys.stdin.readline()) - 1
+        streams[m] += streams[m + 1]
+        streams.pop(m + 1)
+        
+for i in range(len(streams)):
+    print(int(round(streams[i])), end=" ")
+    
