066ea18faf9ab371e1bada76099d3037f9c04832
Roy Lu <Roylu0404@gmail.com>
2026-05-19 09:11:16 -0400

    Create 69A - Young Physicist.cpp

---
+++ Codeforces/69A - Young Physicist.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n, x, y, z;
+    cin >> n;
+    int totalx = 0, totaly= 0, totalz = 0;
+
+    for (int i = 0; i < n ; i++) {
+        cin >> x >> y >> z;
+        totalx += x;
+        totaly += y;
+        totalz += z;
+    }
+
+    if (totalx == 0 && totaly == 0 && totalz == 0){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
